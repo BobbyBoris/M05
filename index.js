acac2094ad92bc4e5fb464a526a228ef004cc592
bobby <114061942+BobbyBoris@users.noreply.github.com>
2022-10-24 12:25:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
 const hostname = '127.0.0.1';
-const port = 420;
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  const data = fs.readFileSync("index.html");
-  res.end(data)
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    const data = fs.readFileSync("index.html");
+    res.end(data)
 
-  res.write("<p>Simple Text</p>");
+    res.write("<p>Simple Text</p>");
 });
 
-server.listen(port, hostname, ()=> {
-  console.log(`Server running at ${hostname}:${port}`);
+server.listen(port,hostname, () =>{
+    console.log(`Server running at ${hostname}:${port}`);
 });
